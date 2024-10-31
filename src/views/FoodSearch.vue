5a0a62246435b800be185488fd3ed028bfbb754f
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2024-10-31 11:41:45 +0500

    add slot

--- src/views/FoodSearch.vue
+++ src/views/FoodSearch.vue
@@ -26,7 +26,16 @@ onMounted(() => {
 
 <template>
     <search-banner>
+        <template #slot1>
+            <div>Slot 1</div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate at laboriosam quasi nihil inventore aspernatur facere, minima, temporibus distinctio possimus alias ullam dolorem veritatis modi cum illum! Praesentium, reprehenderit provident debitis aut nam placeat perspiciatis totam minima quos eveniet officia iure adipisci eligendi voluptatum quia esse laudantium eius consequuntur. Quasi corrupti similique explicabo sunt, doloribus laboriosam. Magnam, quibusdam distinctio! Quibusdam, aut reprehenderit beatae amet repellendus impedit laudantium vitae ullam ipsum. Facere aut magnam est quis sunt itaque. Laboriosam ducimus repellendus quis sed repellat a quo, qui dolore non provident suscipit libero accusamus excepturi itaque iste officiis corporis magnam, totam praesentium!
+            </p>
+        </template>
         <h2>Foods on Your Search <a href="#" class="text-white">"{{ search }}"</a></h2>
+       <template #slot3>
+        <div>slot 3</div>
+       </template>
     </search-banner>
 
     <!-- fOOD MEnu Section Starts Here -->
