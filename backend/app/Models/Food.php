885d1b5a1f0c3533a774aaaecf7c453d1d4bfbe1
Husnain81 <163633770+Husnain81@users.noreply.github.com>
2024-11-21 12:08:13 +0500

    API for food and foodmenu

---
+++ backend/app/Models/Food.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class Food extends Model
+{
+    use HasFactory, HasApiTokens;
+
+    protected $fillable = [
+        'img',
+        'title',
+        'slug',
+    ];
+
+    // protected $hidden = ['img'];
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,10 +4,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FoodController;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\FoodMenuController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/foods', [FoodController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/store', [FoodController::class, 'store']);
+
+Route::get('/foodmenu', [FoodMenuController::class, 'index']);
+Route::post('/foodmenustore', [FoodMenuController::class, 'store']);
+
+// Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
 Route::get('/user', [AuthController::class, 'getUser'])->middleware('auth:sanctum');
 
