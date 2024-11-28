ec171be52a783fbbc749770f61a8d1f408c64c2a
Naraish Kumar <rajnaresh329@gmail.com>
2024-11-28 15:12:17 +0500

    fetch food and food menu on fronted

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,7 +10,9 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Auth routes
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'getUser']);
 
